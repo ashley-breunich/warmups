ae88a99fd3be51ec194221d63b5f48572ef6bb17
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-12-15 15:03:58 -0800

    Initial commit

---
+++ practice-linked-list.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Node = require('./node.js');
+
+class LinkedList {
+
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+
+}
+
+module.exports = LinkedList; 
