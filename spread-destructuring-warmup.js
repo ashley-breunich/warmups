ad603f283b7f0719e84fa673fb1f3df671995ad3
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-12-19 19:10:47 -0800

    descructuring working

--- spread-destructuring-warmup.js
+++ spread-destructuring-warmup.js
@@ -1,5 +1,8 @@
 const people = ['Kookla','Fran','Ollie'];
 
+// Destructuring Assingment & Spread Syntax on people
+let newPeople = ['Odie', ...people, 'Garfield'];
+
 const stuff = {
   tv: 'huge',
   radio: 'old',
@@ -7,8 +10,19 @@ const stuff = {
   cars: ['Toyota','Mazda']
 }
 
-let state = {};
+// Destructuring Assingment & Spread Syntax on stuff
+let newStuff = {...stuff};
+newStuff.cars = [...newStuff.cars, 'Mini Cooper'];
 
-let newPeople = [];
-const newStuff = {};
-let newState = {};
+let state = {...[people], ...stuff};
+state.people
+
+let newState = {...[['Odie', ...people, 'Garfield']], ...{...stuff}};
+newState.cars = [...newState.cars, 'Mini Cooper'];
+
+console.log('original people', people)
+console.log('new people', newPeople);
+console.log('original stuff', stuff);
+console.log('new stuff', newStuff);
+console.log('state', state);
+console.log('new state', newState);
