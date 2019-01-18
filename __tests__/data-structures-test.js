9514d282ab503fdafda88f1e7405f2cb754387c2
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2019-01-17 20:03:19 -0800

    finish up daily warmup

--- __tests__/data-structures-test.js
+++ __tests__/data-structures-test.js
@@ -128,43 +128,43 @@ describe('Linked List Traversal', () => {
     });
   });
 
-// let one = new Node (1);
-// let two = new Node (2);
-// let three = new Node (3);
-// let four = new Node (4);
-// let five = new Node (5);
-// let six = new Node (6);
-// let seven = new Node (7);
+let one = new Node (1);
+let two = new Node (2);
+let three = new Node (3);
+let four = new Node (4);
+let five = new Node (5);
+let six = new Node (6);
+let seven = new Node (7);
 
-// one.left = two;
-// one.right = three;
-// two.left = four;
-// two.right = five;
-// three.left = six;
-// three.right = seven;
+one.left = two;
+one.right = three;
+two.left = four;
+two.right = five;
+three.left = six;
+three.right = seven;
 
-// let tree = new BinaryTree(one);
+let tree = new BinaryTree(one);
 
-// describe('Binary Traversal Trees', () => {
+describe('Binary Tree Traversals', () => {
 
-//     it('Can perform a preOrder traversal', () => {
-//       let preOrderTraversal = tree.preOrder();
-//       expect(preOrderTraversal).toEqual([1, 2, 4, 5, 3, 6, 7]);
-//     });
+    it('Can perform a preOrder traversal', () => {
+      let preOrderTraversal = tree.preOrder();
+      expect(preOrderTraversal).toEqual([1, 2, 4, 5, 3, 6, 7]);
+    });
   
-//     it('Can perform an inOrder traversal', () => {
-//       let inOrderTraversal = tree.inOrder();
-//       expect(inOrderTraversal).toEqual([ 4, 2, 5, 1, 6, 3, 7 ]);
-//     });
+    it('Can perform an inOrder traversal', () => {
+      let inOrderTraversal = tree.inOrder();
+      expect(inOrderTraversal).toEqual([ 4, 2, 5, 1, 6, 3, 7 ]);
+    });
   
-//     it('Can perform a postOrder traversal', () => {
-//       let postOrderTraversal = tree.postOrder();
-//       expect(postOrderTraversal).toEqual([ 4, 5, 2, 6, 7, 3, 1 ]);
-//     });
+    it('Can perform a postOrder traversal', () => {
+      let postOrderTraversal = tree.postOrder();
+      expect(postOrderTraversal).toEqual([ 4, 5, 2, 6, 7, 3, 1 ]);
+    });
   
-//     it('Can perform a breadthFirst traversal', () => {
-//       let BreadthFirstTraversal = tree.breadthFirst();
-//       expect(BreadthFirstTraversal).toEqual([ 1, 2, 3, 4, 5, 6, 7 ]);
-//     });
+    it('Can perform a breadthFirst traversal', () => {
+      let BreadthFirstTraversal = tree.breadthFirst();
+      expect(BreadthFirstTraversal).toEqual([ 1, 2, 3, 4, 5, 6, 7 ]);
+    });
     
-//   });
+  });
